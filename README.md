5b81b3664fe6e13cbf1fde8d17ed22e3c2a8a4d7
glynk <glyn_knight@hotmail.com>
2022-09-10 20:25:34 +0100

    con rendering of big card but json server not working to test

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Job Progress 
+
+// npx json-server -p 8000 -w data/db.json
--- src/Components/Job.js
+++ src/Components/Job.js
@@ -1,10 +1,20 @@
-
+import {useState} from 'react'
 const Job = ({jobTitle, company, link}) => {
-    return <div className="job">
+const [bigCard , setBigCard] = useState(false)
+
+    return <><div className="job" onClick={()=> setBigCard(true)}>
     <h3>{jobTitle}</h3>
     <p>{company}</p>
     <a href={link}>Link to Post</a>
     </div>
+    {(bigCard === true) ?<div className='bigCard'>
+        <h3>{jobTitle}</h3>
+        <p>{company}</p>
+        <a href={link}>Link to Post</a>
+        <button onClick={()=>setBigCard(false)}> Close</button>
+    </div>: null}
+    </>
+
 }
 
 export default Job
